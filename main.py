812404c8123cc01b425d6407e78cacae6b0b5885
muzi981 <76546380+muzi981@users.noreply.github.com>
2022-09-19 11:35:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ def get_words1():
 
 #随机颜色2
 def get_random_color():
-  colorArr = ['1','2','3','4.0','5','6','7','8','9','A','B','C','D','E','F']
+  colorArr = ['1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
   color = ""
   for i in range(6):
       color += colorArr[random.randint(0,14)]
